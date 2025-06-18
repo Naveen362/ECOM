7f9492ff3d5fe19bb881e0f73796b1737ab0f9d9
Naveen sai <naveensaikammela143@gmail.com>
2025-06-18 19:04:31 +0530

    update

--- src/ordercomponents/home/home.jsx
+++ src/ordercomponents/home/home.jsx
@@ -11,7 +11,7 @@ function Home() {
  
     return (
     <div className="container-fluid mt-4 rounded-3 shadow-lg ">
-    <h1 className='fst-italic text-center my-5'>Welcome to E-Commerce🛒 Website</h1>
+    <h1 className='fst-italic text-center my-5 h11'>Welcome to E-Commerce🛒 Website</h1>
         {data.length>0?
         <div className='d-flex naveen flex-wrap justify-content-evenly rounded-5 gap-3'>
       {data.map((data,index)=>{
